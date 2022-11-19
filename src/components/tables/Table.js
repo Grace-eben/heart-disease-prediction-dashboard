4b1ff6abf07b08c2f5e6471bec2c8fd0c35dc79b
R <abiraj-rajendran@hpe.com>
2022-11-19 22:11:50 +0530

    all records ui

--- src/components/tables/Table.js
+++ src/components/tables/Table.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-export const Table = () => {
+export const Table = ({records}) => {
+    console.log("inside ",records)
   return (
 <div class="overflow-x-auto relative shadow-md sm:rounded-lg m-10">
     <table class="w-full text-sm text-left text-gray-700 dark:text-gray-400">
@@ -13,25 +14,22 @@ export const Table = () => {
                     </div>
                 </th>
                 <th scope="col" class="py-3 px-6">
-                    Product name
+                    S.No.
                 </th>
                 <th scope="col" class="py-3 px-6">
-                    Color
+                    Name
                 </th>
                 <th scope="col" class="py-3 px-6">
-                    Category
+                    ID
                 </th>
                 <th scope="col" class="py-3 px-6">
-                    Accesories
+                    Email
                 </th>
                 <th scope="col" class="py-3 px-6">
-                    Available
+                    Date
                 </th>
                 <th scope="col" class="py-3 px-6">
-                    Price
-                </th>
-                <th scope="col" class="py-3 px-6">
-                    Weight
+                    Time
                 </th>
                 <th scope="col" class="py-3 px-6">
                     Action
@@ -39,72 +37,45 @@ export const Table = () => {
             </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b  hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="p-4 w-4">
-                    <div class="flex items-center">
-                        <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600  rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-                        <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                    </div>
-                </td>
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900  whitespace-nowrap hover:text-white">
-                    Apple MacBook Pro 17"
-                </th>
-                <td class="py-4 px-6">
-                    Sliver
-                </td>
-                <td class="py-4 px-6">
-                    Laptop
-                </td>
-                <td class="py-4 px-6">
-                    Yes
-                </td>
-                <td class="py-4 px-6">
-                    Yes
-                </td>
-                <td class="py-4 px-6">
-                    $2999
-                </td>
-                <td class="py-4 px-6">
-                    3.0 lb.
-                </td>
-                <td class="flex items-center py-4 px-6 space-x-3">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                    <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</a>
-                </td>
-            </tr>
-            <tr class="bg-white border-b hover:bg-gray-50 dark:hover:bg-gray-600">
+           
+
+                {
+                    
+                    records.map((record,i)=>{
+
+return <tr class="bg-white border-b hover:bg-gray-50 dark:hover:bg-gray-600">
                 <td class="p-4 w-4">
                     <div class="flex items-center">
                         <input id="checkbox-table-search-2" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         <label for="checkbox-table-search-2" class="sr-only">checkbox</label>
                     </div>
                 </td>
-                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap hover:text-white">
-                    Microsoft Surface Pro
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900  whitespace-nowrap hover:text-white">
+                    {i+1}
                 </th>
                 <td class="py-4 px-6">
-                    White
+                  {record.name}
                 </td>
                 <td class="py-4 px-6">
-                    Laptop PC
+                    {record.id}
                 </td>
                 <td class="py-4 px-6">
-                    No
+                   {record.email}
                 </td>
                 <td class="py-4 px-6">
-                    Yes
+                   {record.date}
                 </td>
                 <td class="py-4 px-6">
-                    $1999
-                </td>
-                <td class="py-4 px-6">
-                    1.0 lb.
+                    {record.time}
                 </td>
                 <td class="flex items-center py-4 px-6 space-x-3">
-                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">View</a>
                     <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</a>
                 </td>
             </tr>
+
+                    })
+                }
           
          
            
--- src/pages/AllPatientHistory.js
+++ src/pages/AllPatientHistory.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import NavBar from '../components/navbar/NavBar'
 import Header from '../components/header/Header'
 import LeftPart from '../components/leftPart/LeftPart'
@@ -6,8 +6,35 @@ import RightPart from './RightPart'
 import { BarChart } from '../components/charts/BarChart'
 import { Table } from '../components/tables/Table'
 import { LineChart } from '../components/charts/LineChart'
+import { useCookies } from 'react-cookie';
 
 export const AllPatientHistory = () => {
+  const [allrecords,setAllrecords] = useState([])
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const token = cookies.token
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/doctor/all-records',{
+                method: 'POST',
+             body: JSON.stringify({
+                token:token
+             }),
+             headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+             },
+          })
+             .then((res) => res.json())
+             .then((res) => {
+              console.log(res)
+              setAllrecords(res.result)
+             })
+             .catch((err) => {
+          console.log(err)
+                console.log(err.message);
+             });
+    },[])
+
+
   return (
     <div className="App overflow-y-hidden ">
      <Header/>
@@ -15,7 +42,7 @@ export const AllPatientHistory = () => {
      <NavBar/>
      <div className='grid grid-cols- col-span-10 w-full'>
       <LineChart/>
-      <Table/>
+      <Table records={allrecords}/>
      </div>
      </div>
     </div>
